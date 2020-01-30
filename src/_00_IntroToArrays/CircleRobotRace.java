cf7c166811c266bd1676a388967f2d48bf7ef28f
League Student <league@iMac-23.attlocal.net>
2020-01-29 20:01:09 -0800

    robotrace

--- src/_00_IntroToArrays/CircleRobotRace.java
+++ src/_00_IntroToArrays/CircleRobotRace.java
@@ -1,18 +1,20 @@
 package _00_IntroToArrays;
 
+
+
 import java.util.Random;
 
 import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
 
-public class _01_RobotRace {
+public class CircleRobotRace {
 	//1. make a main method
 	public static void main(String[] args) {
 		
 	
 		//2. create an array of 5 robots.
-Robot[] robots = new Robot[400];
+Robot[] robots = new Robot[8];
 		//3. use a for loop to initialize the robots.
 int divido=2000/robots.length;
 for(int i = 0;i<robots.length;i++) {
@@ -20,8 +22,11 @@ for(int i = 0;i<robots.length;i++) {
 	robots[i].setY(800);
 	robots[i].setX(divido*i+divido/2);
 	robots[i].setSpeed(500);
-	robots[i].miniaturize();
 }
+
+bottomrob-toprob/rob.length-1
+
+
 			//4. make each robot start at the bottom of the screen, side by side, facing up
 	
 		//5. use another for loop to iterate through the array and make each robot move 
@@ -35,16 +40,16 @@ while(raceOver==false) {
 for(int i = 0;i<robots.length;i++) {
 	
 	
-	robots[i].move(ran.nextInt(200));
-		
-
-	if(robots[i].getY()<=0) {
-		raceOver=true;
-		break;
+		for(int k=0;k<ran.nextInt(150);k++) {
+		robots[i].move(1);
+		robots[i].turn(-1);
 	}
 
-	}
+if(robots[i].getY()<=0) {
+	raceOver=true;
 }
+
+	}
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 		//7. declare that robot the winner and throw it a party!
@@ -86,4 +91,6 @@ for(int i = 0;i<robots.length;i++) {
     	
 	}
 	}
+}
+
 
