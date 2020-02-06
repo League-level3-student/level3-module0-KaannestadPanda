3f19258b6bb813bf09ab80d40430e5a63ab3052f
League Student <league@iMac-109.attlocal.net>
2020-02-05 20:02:37 -0800

    circlerobotrace

--- src/_00_IntroToArrays/CircleRobotRace.java
+++ src/_00_IntroToArrays/CircleRobotRace.java
@@ -12,19 +12,25 @@ public class CircleRobotRace {
 	//1. make a main method
 	public static void main(String[] args) {
 		
+		int startX=800;
+		int startY=600;
 	
 		//2. create an array of 5 robots.
 Robot[] robots = new Robot[8];
+int verticalSpacer=startY/2/(2*(robots.length));  300
 		//3. use a for loop to initialize the robots.
-int divido=2000/robots.length;
+
 for(int i = 0;i<robots.length;i++) {
 	robots[i]=new Robot();
-	robots[i].setY(800);
-	robots[i].setX(divido*i+divido/2);
-	robots[i].setSpeed(500);
+	robots[i].setY(startY+verticalSpacer*i); 600
+	robots[i].setX(startX);
+	robots[i].setSpeed(100);
+	robots[i].turn(90);
+	robots[i].miniaturize();
+	robots[i].penDown();
 }
 
-bottomrob-toprob/rob.length-1
+
 
 
 			//4. make each robot start at the bottom of the screen, side by side, facing up
@@ -38,51 +44,51 @@ Random ran = new Random();
 while(raceOver==false) {
 	
 for(int i = 0;i<robots.length;i++) {
+	robots[i].setSpeed(7000);
 	
-	
-		for(int k=0;k<ran.nextInt(150);k++) {
-		robots[i].move(1);
+		for(int k=0;k<ran.nextInt(450);k++) {
+		robots[i].move(i+1);
 		robots[i].turn(-1);
 	}
 
-if(robots[i].getY()<=0) {
-	raceOver=true;
+if(robots[i].getX()==startX&&robots[i].getY()<=) {
+	//raceOver=true;
 }
 
 	}
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 		//7. declare that robot the winner and throw it a party!
-    	for(int i = 0;i<robots.length;i++) {
-    		if(robots[i].getY()<=0) {
-    			robots[i].setSpeed(200);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			robots[i].move(-600);
-    			robots[i].move(600);
-    			
-    		}
-    	}
+//    	for(int i = 0;i<robots.length;i++) {
+//    		if(robots[i].getY()<=0) {
+//    			robots[i].setSpeed(200);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			robots[i].move(-600);
+//    			robots[i].move(600);
+//    			
+//    		}
+//    	}
 		//8. try different races with different amounts of robots.
     	
 	    //9. make the robots race around a circular track.
